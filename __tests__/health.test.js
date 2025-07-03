0d2732af4a9f37466c9e209ea0443daad71a8266
Sofia <sofjazaitzeva@yandex.ru>
2025-07-03 22:38:17 +0300

    init

---
+++ __tests__/health.test.js
@@ -0,0 +1,13 @@
+import healthStatus from '../src/health';
+
+test('returns healthy if health > 50', () => {
+  expect(healthStatus({ name: 'маг', health: 90 })).toBe('healthy');
+});
+
+test('returns wounded if health between 15 and 50', () => {
+  expect(healthStatus({ name: 'лучник', health: 30 })).toBe('wounded');
+});
+
+test('returns critical if health < 15', () => {
+  expect(healthStatus({ name: 'мечник', health: 10 })).toBe('critical');
+});
